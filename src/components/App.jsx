01a92055b614c804eaf68ba7c0fed9b25afe11e3
jahroost <claysmart@gmail.com>
2017-10-19 11:03:07 -0500

    videoplayer now renders the clicked video from videoList

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,43 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div>
-          <VideoPlayer video={window.exampleVideoData[0]} />
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      videoData: window.exampleVideoData,
+      currentVideo: window.exampleVideoData[0]
+    };
+  }
+
+  onVideoListEntryClick(video) {
+    // console.log(video)
+    this.setState({
+      currentVideo: video
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div>
+              <VideoPlayer video={this.state.currentVideo} />
+            </div>
+          </div>
+          <div className="col-md-5" >
+            <VideoList videos={this.state.videoData} clickFunction={this.onVideoListEntryClick.bind(this)}/>
+          </div>
         </div>
       </div>
-      <div className="col-md-5">
-        <VideoList videos={window.exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,7 +1,7 @@
 var VideoList = (props) => (
   <div>
     {props.videos.map(video =>
-      <VideoListEntry video={video} />
+      <VideoListEntry video={video} clickFunction={props.clickFunction}/>
     )}
   </div>
 );
