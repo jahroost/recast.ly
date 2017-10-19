699d6f320a9538119b8548071da9263e14d0380f
jahroost <claysmart@gmail.com>
2017-10-18 19:57:21 -0500

    loading Rick Astley - need to make this dynamic and NOT STATIC :)

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,8 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+var VideoList = (props) => (
+  <div>
+    {props.videos.map(video =>
+      <VideoListEntry video={video} />
+    )}
   </div>
 );
 
